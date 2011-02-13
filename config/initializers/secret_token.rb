1768a64c9e9627f00e1ee31221e43ac539918a00
Javi Dolcet <austorm@gmail.com>
2011-02-13 17:38:08 +0100

    Added default rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bestbcn::Application.config.secret_token = '501e464eedf1038b8eccf8b5fb751db92e7962124649843b4f8631594f1ade080e8412332d48bf006c521dd80fab85048e997325de46fb4284d29273d81d1c84'
